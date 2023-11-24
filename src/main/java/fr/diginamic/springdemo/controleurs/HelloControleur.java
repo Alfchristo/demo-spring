2e3770754b515342cfac384e73757f8560b66dd4
Alfchristo <juppchristophe@hotmail.com>
2023-11-24 10:43:26 +0100

    TP 1

---
+++ src/main/java/fr/diginamic/springdemo/controleurs/HelloControleur.java
@@ -0,0 +1,19 @@
+package fr.diginamic.springdemo.controleurs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloControleur {
+    @GetMapping
+    public String direHello(){
+        return "Hello";
+    }
+
+    @GetMapping("/salut")
+    public String direSalut(){
+        return "Salut";
+    }
+}
