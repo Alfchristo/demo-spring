fcc222a5ea8f10ae1da7d770eaf1f63cc7937279
Alfchristo <juppchristophe@hotmail.com>
2023-11-27 15:22:41 +0100

    TP 5.5

--- src/main/java/fr/diginamic/springdemo/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/springdemo/controleurs/VilleControleur.java
@@ -1,8 +1,11 @@
 package fr.diginamic.springdemo.controleurs;
 import fr.diginamic.springdemo.entites.Ville;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/villes")
@@ -26,32 +29,42 @@ public class VilleControleur {
     }
 
     @PutMapping
-    public String insertVille(@RequestBody Ville nvVille) {
+    public ResponseEntity<String> insertVille(@RequestBody Ville nvVille) {
         if (!villes.contains(nvVille)) {
             villes.add(nvVille);
-            return "Ville insérée avec succès";
+            return ResponseEntity.ok("Ville insérée avec succès");
         }
-        return "non";
+        return ResponseEntity.badRequest().body("La ville existe déjà");
     }
 
     @PostMapping("/{id}")
-    public Ville modifierVille(@PathVariable Integer id, @RequestBody Ville nVille){
-        for (Ville v: villes){
+    public ResponseEntity<String> modifierVille(@PathVariable int id, @RequestBody Ville nVille){
+       if (id<=0){
+           return ResponseEntity.badRequest().body("Identifiant incorrect");
+       }
+       for (Ville v: villes){
             if (v.getId()==id){
                 v.setNom(nVille.getNom());
                 v.setNbHabitant(nVille.getNbHabitant());
-                return v;
+                return ResponseEntity.ok("Ville modifier avec succes");
             }
         }
-        return null;
+        return ResponseEntity.badRequest().body("Erreur, identifiant: "+id+"non trouver");
     }
 
     @DeleteMapping("/{id}")
-    public void deleteVille(@PathVariable Integer id){
-        Ville v = getVilleId(id);
-        if (v == null){
-            return;
+    public ResponseEntity<String> deleteVille(@PathVariable int id) {
+        if (id <= 0) {
+            return ResponseEntity.badRequest().body("L'identifiant est incorrect");
         }
-        villes.remove(v);
+        Iterator<Ville> iterVilles = villes.iterator();
+        while (iterVilles.hasNext()) {
+            Ville v = iterVilles.next();
+            if (v.getId() == id) {
+                iterVilles.remove();
+                return ResponseEntity.ok("Ville supprimé");
+            }
+        }
+        return ResponseEntity.badRequest().body("La ville avec l'id "+ id + "n'a pas été trouvée");
     }
 }
